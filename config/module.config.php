ad86dcfcb33b416061ffac49c8b1008a55f43082
Justin Martin <frozenfire@thefrozenfire.com>
2013-11-24 18:05:17 -0800

    Added ZfcUser config.

--- config/module.config.php
+++ config/module.config.php
@@ -3,4 +3,5 @@ return array(
     'controllers' => require __DIR__.'/module/controllers.config.php',
     'router' => require __DIR__.'/module/router.config.php',
     'view_manager' => require __DIR__.'/module/view_manager.config.php',
+    'zfcuser' => require __DIR__.'/module/zfcuser.config.php',
 );
---
+++ config/module/zfcuser.config.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+    'enable_registration' => true,
+    'login_after_registration' => true,
+);
